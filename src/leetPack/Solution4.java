063fc038a903a7cf3bec5e18725060287c840af4
iqorange <39493393+iqorange@users.noreply.github.com>
2020-03-06 22:53:57 +0800

    leet0306

--- src/leetPack/Solution4.java
+++ src/leetPack/Solution4.java
@@ -17,8 +17,94 @@ public class Solution4 {
                 car = car.next;
                 point = point.next;
             }
-
         }
         return point;
+    }
+
+    // 191. 位1的个数
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+
+    // 面试题52. 两个链表的第一个公共节点
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode node1=headA;
+        ListNode node2=headB;
+        while(node1!=node2){
+            node1=node1==null?headB:node1.next;
+            node2=node2==null?headA:node2.next;
+        }
+        return node1;
+    }
+
+    // 面试题 05.07. 配对交换
+    public int exchangeBits(int num) {
+        String s = Integer.toBinaryString(num);
+        StringBuilder str = new StringBuilder();
+        int i=0;
+        if (s.length()%2!=0){
+            str.append(s.charAt(0));
+            str.append('0');
+            i = 1;
+        }
+        for (;i<s.length();i+=2){
+            str.append(s.charAt(i+1));
+            str.append(s.charAt(i));
+        }
+        return Integer.parseInt(str.toString(), 2);
+    }
+
+    // 669. 修剪二叉搜索树
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) return null;
+        if (root.val<L){
+            return trimBST(root.right, L, R);
+        }
+        if (root.val>R){
+            return trimBST(root.left, L, R);
+        }
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
+    }
+
+    // 107. 二叉树的层次遍历 II
+    List<List<Integer>> lists = new LinkedList<>();
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        levelOrderBottom(root, 0);
+        Collections.reverse(lists);
+        return lists;
+    }
+    private void levelOrderBottom (TreeNode root, int depth) {
+        if (root == null) return;
+        if (lists.size()<=depth) lists.add(new LinkedList<>());
+        levelOrderBottom(root.left, depth+1);
+        levelOrderBottom(root.right, depth+1);
+        lists.get(depth).add(root.val);
+    }
+
+    // 806. 写字符串需要的行数
+    public int[] numberOfLines(int[] widths, String S) {
+        String abc = "abcdefghijklmnopqrstuvwxyz";
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i=0;i<widths.length;i++){
+            map.put(abc.charAt(i), widths[i]);
+        }
+        int[] result = new int[2];
+        int length = 0;
+        int row = 1;
+        for (int i=0;i<S.length();i++){
+            int t = map.get(S.charAt(i));
+            if (t+length>100){
+                row++;
+                length = t;
+            }else{
+                length += t;
+            }
+        }
+        result[0] = row;
+        result[1] = length;
+        return result;
     }
 }
