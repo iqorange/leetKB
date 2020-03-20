23e794a1a7a16cb0da157572f1d9834f2d87b853
iqorange <39493393+iqorange@users.noreply.github.com>
2020-03-20 22:07:16 +0800

    303Range

--- src/SegmentTree/SegmentTree.java
+++ src/SegmentTree/SegmentTree.java
@@ -65,6 +65,32 @@ public class SegmentTree<E> {
         return 2*index+2;
     }
 
+    // 查询某个区间前闭后闭的的值
+    public E query(int queryL, int queryR){
+        if (queryL > queryR || queryL<0 || queryR>=data.length){
+            throw new IllegalArgumentException("Index is illegal.");
+        }
+        return query(0, 0, data.length-1, queryL, queryR);
+    }
+
+    private E query(int treeIndex, int l, int r, int queryL, int queryR){
+        if (l==queryL && r==queryR){
+            return tree[treeIndex];
+        }
+        int mid = l+(r-l) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        if (queryL >= mid+1){
+            return query(rightTreeIndex, mid+1, r, queryL, queryR);
+        }else if (queryR <= mid){
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+        }else{
+            E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+            E rightResult = query(rightTreeIndex, mid+1, r, mid+1, queryR);
+            return merger.merge(leftResult, rightResult);
+        }
+    }
+
     // 重写toString打印输出
     @Override
     public String toString() {
