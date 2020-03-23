bfd2048e7585be09043092249fe3e6651151a9ae
iqorange <39493393+iqorange@users.noreply.github.com>
2020-03-23 09:44:15 +0800

    Trie Simple finished

--- src/Trie/Trie.java
+++ src/Trie/Trie.java
@@ -4,6 +4,7 @@ package Trie;
 import java.util.Map;
 import java.util.TreeMap;
 
+// 208. 实现 Trie (前缀树)
 // 字典树Trie-使用TreeMap辅助实现
 public class Trie {
     // 创建私有节点类
@@ -42,11 +43,12 @@ public class Trie {
     }
 
     // 添加字符串单词，拆分字符到Trie中
-    public void add(String word){
+    /** Initialize your data structure here. */
+    public void insert(String word){
         Node cur = root;
         for (int i=0;i<word.length();i++){
             char c = word.charAt(i);
-            if (!cur.next.containsKey(c)){
+            if (cur.next.get(c) == null){
                 cur.next.put(c, new Node());
             }
             cur = cur.next.get(c);
@@ -57,4 +59,40 @@ public class Trie {
             size++;
         }
     }
+
+    // 查询单词是否存在
+    /** Returns if the word is in the trie. */
+    public boolean search(String word){
+        Node cur = root;
+        for (int i=0;i<word.length();i++){
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null){
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    // 前缀搜索
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix){
+        Node cur = root;
+        for (int i=0;i<prefix.length();i++){
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null){
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
