eb9714f4a2223759abb9f49317dc380d9c20786b
iqorange <39493393+iqorange@users.noreply.github.com>
2020-03-27 22:39:12 +0800

    HashTable simple finished

---
+++ src/HashTable/Main.java
@@ -0,0 +1,16 @@
+package HashTable;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 65;
+        System.out.println(Integer.hashCode(a));
+        int b = -65;
+        System.out.println(Integer.hashCode(b));
+        double c = 3.1415926;
+        System.out.println(Double.hashCode(c));
+        String d = "Hello!";
+        System.out.println(d.hashCode());
+        Student student = new Student(2,1,"kb");
+        System.out.println(student.hashCode());
+    }
+}
