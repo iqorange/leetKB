7f62f84afe69943bd50b3f8ee0d1f0ed67b057c3
iqorange <39493393+iqorange@users.noreply.github.com>
2020-04-24 07:12:42 +0800

    leet0424

---
+++ leetPack/PeekingIterator.java
@@ -0,0 +1,36 @@
+package leetPack;
+
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class PeekingIterator implements Iterator<Integer> {
+    Queue<Integer> queue;
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        queue = new LinkedList<>();
+        while (iterator.hasNext()){
+            queue.add(iterator.next());
+        }
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return queue.peek();
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        return queue.remove();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !queue.isEmpty();
+    }
+}
