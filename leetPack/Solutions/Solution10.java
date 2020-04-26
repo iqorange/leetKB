56fc42ed45fb4ba5fc9359abc91ec88f9462a27f
iqorange <39493393+iqorange@users.noreply.github.com>
2020-04-26 08:09:52 +0800

    leet0426

--- leetPack/Solutions/Solution10.java
+++ leetPack/Solutions/Solution10.java
@@ -1,5 +1,8 @@
 package leetPack.Solutions;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution10 {
     // 1008. 先序遍历构造二叉树
     public TreeNode bstFromPreorder(int[] preorder) {
@@ -19,5 +22,29 @@ public class Solution10 {
             root.right = bstFromPreorder(root.right, num);
         }
         return root;
+    }
+
+    // 230. 二叉搜索树中第K小的元素
+    private int step = 0, value = -1;
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null){
+            return value;
+        }
+        kthSmallest(root.left, k);
+        step++;
+        if (step == k){
+            value = root.val;
+            return value;
+        }
+        kthSmallest(root.right, k);
+        return value;
+    }
+
+    // 222. 完全二叉树的节点个数
+    public int countNodes(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        return countNodes(root.left) + countNodes(root.right) + 1;
     }
 }
