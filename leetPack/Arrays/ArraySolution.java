e76242844535525322b673cab3cb8299c4de9753
iqorange <39493393+iqorange@users.noreply.github.com>
2020-05-19 19:41:17 +0800

    leet0519

--- leetPack/Arrays/ArraySolution.java
+++ leetPack/Arrays/ArraySolution.java
@@ -1,5 +1,8 @@
 package leetPack.Arrays;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class ArraySolution {
     // 283. 移动零
     public void moveZeroes(int[] nums) {
@@ -64,5 +67,81 @@ public class ArraySolution {
             }
         }
         return cc;
+    }
+
+    // 75. 颜色分类
+    public void sortColors(int[] nums) {
+        int[] count = new int[3];
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]>=0 && nums[i]<=2) {
+                count[nums[i]]++;
+            }else{
+                throw new IllegalArgumentException("Error Color~");
+            }
+        }
+        int index = 0;
+        for (int i=0;i<count.length;i++){
+            for (int j = 0;j<count[i];j++){
+                nums[index++] = i;
+            }
+        }
+    }
+    // 解法2(三路快排)
+    public void sortColors2(int[] nums) {
+        int zero = -1;
+        int two = nums.length;
+        for (int i=0;i<two;){
+            if (nums[i] == 1){
+                i++;
+            }else if (nums[i] == 2){
+                two--;
+                int t = nums[i];
+                nums[i] = nums[two];
+                nums[two]  = t;
+            }else if (nums[i] == 0){
+                zero++;
+                int t = nums[i];
+                nums[i] = nums[zero];
+                nums[zero]  = t;
+                i++;
+            }else{
+                throw new IllegalArgumentException("Error Color~");
+            }
+        }
+    }
+
+    // 88. 合并两个有序数组
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // 最小堆的方式
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int i=0;i<m;i++){
+            priorityQueue.add(nums1[i]);
+        }
+        for (int e: nums2){
+            priorityQueue.add(e);
+        }
+        for (int i=0;i<(m+n);i++){
+            nums1[i] = priorityQueue.remove();
+        }
+        priorityQueue = null;
+    }
+    // 解法2
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int p = m-- + n-- - 1;
+        while (m >= 0 && n >= 0) {
+            nums1[p--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
+        while (n >= 0) {
+            nums1[p--] = nums2[n--];
+        }
+    }
+
+    // 215. 数组中的第K个最大元素
+    public int findKthLargest(int[] nums, int k) {
+        if (k>nums.length){
+            throw new IllegalArgumentException("Out of nums length~");
+        }
+        Arrays.sort(nums);
+        return nums[nums.length-k];
     }
 }
