7e6a33d33d6a34b38580c8343f4b38ccf01bfdc6
iqorange <39493393+iqorange@users.noreply.github.com>
2020-05-22 00:05:56 +0800

    fix

--- leetPack/Arrays/ArraySolution.java
+++ leetPack/Arrays/ArraySolution.java
@@ -286,9 +286,9 @@ public class ArraySolution {
         if (s == null || s == "" || t == null || t == "" || s.length() < t.length()) {
             return "";
         }
-        //用来统计t中每个字符出现次数
+        // 用来统计t中每个字符出现次数
         int[] needs = new int[128];
-        //用来统计滑动窗口中每个字符出现次数
+        // 用来统计滑动窗口中每个字符出现次数
         int[] window = new int[128];
         for (int i = 0; i < t.length(); i++) {
             needs[t.charAt(i)]++;
@@ -296,9 +296,9 @@ public class ArraySolution {
         int left = 0;
         int right = 0;
         String res = "";
-        //目前有多少个字符
+        // 目前有多少个字符
         int count = 0;
-        //用来记录最短需要多少个字符。
+        // 用来记录最短需要多少个字符。
         int minLength = s.length() + 1;
         while (right < s.length()) {
             char ch = s.charAt(right);
@@ -306,7 +306,7 @@ public class ArraySolution {
             if (needs[ch] > 0 && needs[ch] >= window[ch]) {
                 count++;
             }
-            //移动到不满足条件为止
+            // 移动到不满足条件为止
             while (count == t.length()) {
                 ch = s.charAt(left);
                 if (needs[ch] > 0 && needs[ch] >= window[ch]) {
@@ -315,7 +315,6 @@ public class ArraySolution {
                 if (right - left + 1 < minLength) {
                     minLength = right - left + 1;
                     res = s.substring(left, right + 1);
-
                 }
                 window[ch]--;
                 left++;
