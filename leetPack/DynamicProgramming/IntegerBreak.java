917f710cd31c36e28f62ce0b1dc7dbcb7c072c28
LiBoran <iqorange@yeah.net>
2020-09-26 20:14:52 +0800

    IntegerBreak

--- leetPack/DynamicProgramming/IntegerBreak.java
+++ leetPack/DynamicProgramming/IntegerBreak.java
@@ -1,27 +1,20 @@
 package leetPack.DynamicProgramming;
 
 public class IntegerBreak {
-    private int[] memo;
-
     public int integerBreak(int n) {
         assert n >= 2;
-        memo = new int[n + 1];
-        return breakInteger(n);
-    }
-
-    private int breakInteger(int n){
-        if (n == 1) return 1;
-        if(memo[n] != -1) return memo[n];
-        int res = -1;
-        for(int i=1;i<n;i++){
-            res = maxAll(1, res, i * (n - i), i * breakInteger(n - i));
+        int[] memo = new int[n + 1];
+        memo[1] = 1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i-1;j++){
+                memo[i] = maxAll(false, memo[i], j * (i - j), j * memo[i-j]);
+            }
         }
-        memo[n] = res;
-        return res;
+        return memo[n];
     }
 
-    private int maxAll(int miniVal, int ...a){
-        int maxVal = miniVal;// 这里只考虑正整数情况
+    private int maxAll(boolean miniVal, int ...a){
+        int maxVal = miniVal ? Integer.MIN_VALUE : 0;
         for(int e : a){
             maxVal = Math.max(maxVal, e);
         }
