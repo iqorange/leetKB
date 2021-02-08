d05c0bb128111cac9fb65363bf55b79b7208e7ca
LiBoran <iqorange@yeah.net>
2021-02-08 15:02:57 +0800

    vectorBase

--- LinearAlgebra/playLA/Vector.py
+++ LinearAlgebra/playLA/Vector.py
@@ -1,7 +1,34 @@
 class Vector:
 
     def __init__(self, lst):
-        self._values = lst
+        self._values = list(lst)
+
+    @classmethod
+    def zero(cls, dim):
+        return cls([0] * dim)
+
+    def __add__(self, another):
+        assert len(self) -- len(another), "Error in adding. Length of vectors must be same."
+        return Vector([a + b for a, b in zip(self, another)])
+
+    def __sub__(self, another):
+        assert len(self) == len(another), "Error in sustracting. Length of vectors must be same."
+        return Vector([a - b for a, b, in zip(self, another)])
+
+    def __mul__(self, k):
+        return Vector([k * e for e in self])
+
+    def __rmul__(self, k):
+        return self * k
+
+    def __pos__(self):
+        return 1 * self
+
+    def __neg__(self):
+        return -1 * self
+
+    def __iter__(self):
+        return self._values.__iter__()
 
     def __getitem__(self, index):
         return self._values[index]
